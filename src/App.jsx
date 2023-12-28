ad9201868bcd1012724484d3d14b4112b6b899db
vinithamuthu <vinisurya.m@gmail.com>
2023-12-28 14:20:34 +0530

    compleated

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+
+import Card from './components/Card'
+
+
+import Header from './components/Header'
+import TopBar from './components/TopBar'
+// import Header from './components/Header'
+
+function App() {
+
+    let [cart,setCart] = useState(0)
+
+
+
+return <>
+
+<TopBar cart={cart} setCart={setCart} />
+<Header/>
+
+{/* <!-- Section--> */}
+        <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                
+                  <Card cart={cart} setCart={setCart} /> 
+                  <Card cart={cart} setCart={setCart} /> 
+                  <Card cart={cart} setCart={setCart} /> 
+                  
+                </div>
+            </div>
+        </section>        
+
+
+
+</>
+
+}
+
+export default App
